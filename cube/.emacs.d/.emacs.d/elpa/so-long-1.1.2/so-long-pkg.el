e9a26c63607671ae098d24909bb9aaaff5b2b0a4
Maskil <makigo613@gmail.com>
2023-05-31 17:15:32 +0900

    05-31

---
+++ cube/.emacs.d/.emacs.d/elpa/so-long-1.1.2/so-long-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from so-long.el  -*- no-byte-compile: t -*-
+(define-package "so-long" "1.1.2" "Say farewell to performance problems with minified code." '((emacs "24.4")) :commit "045a4fe94c18cd36ef297e62a80cdff449af3aa5" :authors '(("Phil Sainty" . "psainty@orcon.net.nz")) :maintainer '("Phil Sainty" . "psainty@orcon.net.nz") :keywords '("convenience") :url "https://savannah.nongnu.org/projects/so-long")
