64ca67e7f953ad886189a7b2faa755241f2cd6e9
Maskil <makigo613@gmail.com>
2023-06-01 05:49:42 +0900

    06-01

--- t14/.emacs.d/compile-modes.el
+++ t14/.emacs.d/compile-modes.el
@@ -2,13 +2,13 @@
 	(lambda()
 		(unless (or (file-exists-p "makefile") (file-exists-p "Makefile"))
 			(set (make-local-variable 'compile-command)
-				(concat "gcc -Wall " (shell-quote-argument buffer-file-name) " && ./a.exe")))))
+				(concat "gcc -Wall " (shell-quote-argument buffer-file-name) " && .\a.exe")))))
 
 (add-hook 'c++-mode-hook
 	(lambda()
 		(unless (or (file-exists-p "makefile") (file-exists-p "Makefile"))
 			(set (make-local-variable 'compile-command)
-				(concat "g++ -std=c++11 -Wall " (shell-quote-argument buffer-file-name) " && ./a.exe")))))
+				(concat "g++ -std=c++11 -Wall " (shell-quote-argument buffer-file-name) " && .\a.exe")))))
 
 (add-hook 'python-mode-hook
 	(lambda()
@@ -19,5 +19,5 @@
   (lambda()
     (set (make-local-variable 'compile-command)
       (concat "pandoc -F pandoc-crossref " buffer-file-name " -o " (file-name-sans-extension buffer-file-name)
-        ".pdf -N --pdf-engine=lualatex --highlight-style monochrome -V documentclass=ltjarticle -V CJKmainfont=SourceHanSerif -V mainfont=SourceHanSerif -V fontsize=11.5pt -V geometry=a4paper -V geometry:margin=1in"))))
+        ".pdf --pdf-engine=lualatex --highlight-style monochrome -V documentclass=ltjarticle -V CJKmainfont=SourceHanSerif -V mainfont=SourceHanSerif -V fontsize=11.5pt -V geometry=a4paper -V geometry:margin=1in --lua-filter=%HOME%/pandoc-config/header.lua"))))
 
