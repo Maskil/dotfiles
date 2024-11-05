e02fdfa9b50a6807debfd0710091565043829b24
maki <makigo@aubsebian.net>
2024-11-06 07:30:04 +0900

    update

--- cube/.emacs.custom.el
+++ cube/.emacs.custom.el
@@ -3,6 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-view-program-selection
+   '(((output-dvi has-no-display-manager)
+      "dvi2tty")
+     ((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "PDF Tools")
+     (output-html "xdg-open")))
  '(compile-command "bash build.sh ")
  '(custom-safe-themes
    '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd" default))
