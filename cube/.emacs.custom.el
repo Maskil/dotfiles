b7480aef30f1de55b78681160dc1a19f1b243450
maki <makigo@aubsebian.net>
2024-12-17 11:35:22 +0900

    update

--- cube/.emacs.custom.el
+++ cube/.emacs.custom.el
@@ -3,24 +3,25 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-engine 'luatex)
+ '(TeX-engine 'default)
  '(TeX-view-program-selection
    '(((output-dvi has-no-display-manager)
       "dvi2tty")
      ((output-dvi style-pstricks)
       "dvips and gv")
      (output-dvi "xdvi")
-     (output-pdf "xdg-open")
+     (output-pdf "PDF Tools")
      (output-html "xdg-open")))
  '(compile-command "bash build.sh ")
  '(custom-safe-themes
    '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd" default))
+ '(ein:jupyter-server-use-subcommand "server")
  '(highlight-indent-guides-auto-character-face-perc 50)
  '(highlight-indent-guides-character 124)
  '(highlight-indent-guides-method 'character)
  '(magit-bury-buffer-function 'magit-restore-window-configuration)
  '(package-selected-packages
-   '(csv-mode company-jedi pdf-tools markdown-mode gruber-darker-theme auctex-latexmk auctex magit dash-functional dash)))
+   '(ein csv-mode company-jedi pdf-tools markdown-mode gruber-darker-theme auctex-latexmk auctex magit dash-functional dash)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
